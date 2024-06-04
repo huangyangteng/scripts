3932e64f52b4946c4ea7a20c7cf1203489b7e399
hyangteng <hyangteng@gmail.com>
2024-06-04 23:41:01 +0800

    feat: clean code

--- pupper-excel/in-db.js
+++ pupper-excel/in-db.js
@@ -1,6 +1,5 @@
 const fs = require('fs')
 const shell = require('shelljs')
-const path = require('path')
 const INPUT_FILE = '/Users/h/Desktop/scripts/pupper-excel/peppa.json'
 
 const data = JSON.parse(fs.readFileSync(INPUT_FILE).toString())
@@ -11,26 +10,25 @@ const sleep = async (milliseconds) => {
 async function fetchAdd(data, i) {
     await sleep(100)
     await shell.exec(
-        `
-    curl -X POST http://localhost:22222/lines/ \
--H "Content-Type: application/json" \
--d '${JSON.stringify(data)}'
-    
+    `curl -X POST http://localhost:22222/lines/ \
+        -H "Content-Type: application/json" \
+        -d '${JSON.stringify(data)}'
     `,
         { silent: true },
         (code, stdout, stderr) => {
             console.log(stdout)
             if (code !== 0) {
-                console.log('🐔🐔🐔', i, data, stderr)
+                console.log('🐔🐔🐔error', i, data, stderr)
             }
         }
     )
 }
 async function inDb() {
     const len = data.length
-    for (let i = 2592; i < len; i++) {
+    for (let i = 0; i < len; i++) {
         console.log(i)
         const item = data[i]
+        //'如果不替换成‘，curl执行时会出现转义问题,出错例子：  'I'm peppa pig'
         item.en = item.en.replace(/'/g, '‘')
         await fetchAdd(item, i)
     }
